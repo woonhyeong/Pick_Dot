16ac3251d1836e6772e45d6b4e4ec338bf3ac22a
woonhyeong <tlsehddkql12@gmail.com>
2018-11-13 23:44:03 +0900

    [added class PixelBrain]
    act as model

---
+++ PickDot/PixelBrain.m
@@ -0,0 +1,13 @@
+//
+//  PixelBrain.m
+//  PickDot
+//
+//  Created by 이운형 on 13/11/2018.
+//  Copyright © 2018 201302458. All rights reserved.
+//
+
+#import "PixelBrain.h"
+
+@implementation PixelBrain
+
+@end
